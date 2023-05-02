015efe34cd1b2af7f47c68e19782486d18cbc2ad
Zanckor <zanckor002@gmail.com>
2023-05-02 16:26:18 +0200

    Updated 1.19.4 to correct version

--- src/main/java/dev/zanckor/example/common/enumregistry/enumdialog/EnumOptionType.java
+++
@@ -1,7 +0,0 @@
-package dev.zanckor.example.common.enumregistry.enumdialog;
-
-public enum EnumOptionType {
-    OPEN_DIALOG,
-    CLOSE_DIALOG,
-    ADD_QUEST
-}
--- src/main/java/dev/zanckor/example/common/enumregistry/enumquest/EnumQuestType.java
+++
@@ -1,9 +0,0 @@
-package dev.zanckor.example.common.enumregistry.enumquest;
-
-
-public enum EnumQuestType {
-    INTERACT_ENTITY,
-    KILL,
-    MOVE_TO,
-    COLLECT
-}
