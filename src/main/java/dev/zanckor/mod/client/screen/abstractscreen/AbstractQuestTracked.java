cb01ba11b7459c047e0a0af84199ae28481c90e2
Zanckor <zanckor002@gmail.com>
2023-08-09 17:28:44 +0200

    Updated from 1.18.2 1.3.9

--- src/main/java/dev/zanckor/mod/client/screen/abstractscreen/AbstractQuestTracked.java
+++
@@ -1,7 +0,0 @@
-package dev.zanckor.mod.client.screen.abstractscreen;
-
-import com.mojang.blaze3d.vertex.PoseStack;
-
-public abstract class AbstractQuestTracked {
-    public abstract void renderQuestTracked(PoseStack poseStack, int width, int height);
-}
